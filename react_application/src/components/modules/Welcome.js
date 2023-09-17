3aeb97cc9d7b496263e406956f4061b79e7665e5
Taehoon Hwang <cadenhwang05@gmail.com>
2023-09-17 03:15:30 -0400

    i need more coffeeeeeeeeee

--- react_application/src/components/modules/Welcome.js
+++ react_application/src/components/modules/Welcome.js
@@ -1,9 +1,18 @@
 import './../../assets/styles/welcomepage_style.css'
 
 export default function Welcome() {
+  const texts = [
+    { id: 1, text: 'Boiler Balance', style: 'title' },
+    { id: 2, text: "Boiler Balance, powered by ChatGPT's analytical prowess, revolutionizes personal nutrition tracking by offering intelligent, personalized feedback on your daily dietary habits. Join us to transform your health journey into an insightful and adaptive experience, tailored to foster a balanced and healthy lifestyle.", style: 'description' },
+  ];
+
   return (
-    <div className="container-welcome-page">
-      <p className="text-welcome-page">placeholder</p>
+    <div className="textContainer">
+      {texts.map(({ id, text, style }) => (
+        <div key={id} className={`textInstance ${style}`}>
+          {text}
+        </div>
+      ))}
     </div>
-  );  
+  );
 }
